13840e747cb8c3bc2ae82097f9879d4fc6f6b8a4
Robel <robel@engineer.com>
2018-03-13 00:38:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function notp() {
+    document.getElementById("content").innerHTML = "\"If I were not a physicist, I would probably be a musician. I often think in music. I live my daydreams in music. I see my life in terms of music.\"-- Albert Einstein";
+}
+
+
+function phy() {
+    document.getElementById("content").innerHTML = "\"The laws of physics are the same for all non-accelerating observers, and that the speed of light in a vacuum is independent of the motion of all observers.\"--Albert Einstein";
+}
