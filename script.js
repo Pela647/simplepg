c5816cfb845a5978d0e8b372c8a60bbd3aa49fe2
Robel <robel@engineer.com>
2018-03-13 00:38:00 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function notp() {
-    alert("\"If I were not a physicist, I would probably be a musician. I often think in music. I live my daydreams in music. I see my life in terms of music.\"-- Albert Einstein");
-}
-
-
-function phy() {
-    alert("\"I have no special talents. I am only passionately curious.\"--Albert Einstein");
-}
